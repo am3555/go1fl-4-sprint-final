b675a0c50863fa10c8bc054290804ec805a8a771
a.mikholap <ac3555@mail.ru>
2025-12-31 15:26:59 +0300

    package update daysteps, spentcalories

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,7 +1,12 @@
 package daysteps
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
 const (
@@ -13,8 +18,62 @@ const (
 
 func parsePackage(data string) (int, time.Duration, error) {
 	// TODO: реализовать функцию
+	splitStr := strings.Split(data, ",")
+
+	if len(splitStr) != 2 {
+		return 0, 0, fmt.Errorf("некорректный формат данных: %s", data)
+	}
+
+	numStr := splitStr[0]
+	durationStr := splitStr[1]
+
+	// Преобразование строки в int
+	num, err := strconv.Atoi(numStr)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	if num <= 0 {
+		return 0, 0, fmt.Errorf("количество шагов %d должно быть больше 0", num)
+	}
+
+	// Преобразование строки в duration
+	duration, err := time.ParseDuration(durationStr)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	if duration <= 0 {
+		return 0, 0, fmt.Errorf("длительность %d должна быть больше 0", duration)
+	}
+
+	return num, duration, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
 	// TODO: реализовать функцию
+	countSteps, duration, err := parsePackage(data)
+	if err != nil {
+		errStirng := fmt.Sprint(err)
+		return errStirng
+	}
+
+	if countSteps <= 0 {
+		return ""
+	}
+
+	distance := float64(countSteps) * stepLength / mInKm
+
+	countCalories, err := spentcalories.WalkingSpentCalories(countSteps, weight, height, duration)
+	if err != nil {
+		errStirng := fmt.Sprint(err)
+		return errStirng
+	}
+
+	result := fmt.Sprintf(`
+Количество шагов: %d.
+Дистанция составила %.2f км.
+Вы сожгли %.2f ккал.`, countSteps, distance, countCalories)
+
+	return result
 }
--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -1,12 +1,16 @@
 package spentcalories
 
 import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
 	"time"
+	//"golang.org/x/text/width"
 )
 
 // Основные константы, необходимые для расчетов.
 const (
-	lenStep                    = 0.65 // средняя длина шага.
 	mInKm                      = 1000 // количество метров в километре.
 	minInH                     = 60   // количество минут в часе.
 	stepLengthCoefficient      = 0.45 // коэффициент для расчета длины шага на основе роста.
@@ -15,24 +19,136 @@ const (
 
 func parseTraining(data string) (int, string, time.Duration, error) {
 	// TODO: реализовать функцию
+	splitStr := strings.Split(data, ",")
+
+	if len(splitStr) != 3 {
+		return 0, "", 0, fmt.Errorf("некорректный формат данных: %s", data)
+	}
+
+	stepsStr := splitStr[0]
+	activeType := splitStr[1]
+	durationStr := splitStr[2]
+
+	// Преобразование строки в int
+	countSteps, err := strconv.Atoi(stepsStr)
+	if err != nil {
+		return 0, "", 0, err
+	}
+
+	if countSteps <= 0 {
+		return 0, "", 0, fmt.Errorf("количество шагов %d должно быть больше 0", countSteps)
+	}
+
+	// Преобразование строки в duration
+	duration, err := time.ParseDuration(durationStr)
+	if err != nil {
+		return 0, "", 0, err
+	}
+
+	if duration <= 0 {
+		return 0, "", 0, fmt.Errorf("длительность %d должна быть больше 0", duration)
+	}
+
+	return countSteps, activeType, duration, nil
 }
 
 func distance(steps int, height float64) float64 {
 	// TODO: реализовать функцию
+	distanceKm := (height * stepLengthCoefficient * float64(steps)) / mInKm
+
+	return distanceKm
 }
 
 func meanSpeed(steps int, height float64, duration time.Duration) float64 {
 	// TODO: реализовать функцию
+	if duration <= 0 {
+		return 0
+	}
+
+	avgSpeed := distance(steps, height) / float64(duration.Hours())
+
+	return avgSpeed
 }
 
 func TrainingInfo(data string, weight, height float64) (string, error) {
 	// TODO: реализовать функцию
+	num, activeType, duration, err := parseTraining(data)
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+
+	switch activeType {
+	case "Ходьба":
+		distanceKm := distance(num, height)
+		avgSpeed := meanSpeed(num, height, duration)
+		countCaloriesWalk, err := WalkingSpentCalories(num, weight, height, duration)
+		if err != nil {
+			return "", err
+		}
+		resultWalk := fmt.Sprintf(`
+Тип тренировки: %s
+Длительность: %.2f ч.
+Дистанция: %.2f км.
+Скорость: %.2f км/ч
+Сожгли калорий: %.2f`, activeType, duration.Hours(), distanceKm, avgSpeed, countCaloriesWalk)
+		return resultWalk, nil
+
+	case "Бег":
+		distanceKm := distance(num, height)
+		avgSpeed := meanSpeed(num, height, duration)
+		countCaloriesRun, err := RunningSpentCalories(num, weight, height, duration)
+		if err != nil {
+			return "", err
+		}
+		resultRun := fmt.Sprintf(`
+Тип тренировки: %s
+Длительность: %.2f ч.
+Дистанция: %.2f км.
+Скорость: %.2f км/ч
+Сожгли калорий: %.2f`, activeType, duration.Hours(), distanceKm, avgSpeed, countCaloriesRun)
+		return resultRun, nil
+
+	default:
+		return "", fmt.Errorf("неизвестный тип тренировки")
+	}
 }
 
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	// TODO: реализовать функцию
+	if steps <= 0 {
+		return 0, fmt.Errorf("количество шагов %d должно быть больше 0", steps)
+	}
+	if weight <= 0 {
+		return 0, fmt.Errorf("вес должен %.2f быть больше 0", weight)
+	}
+	if height <= 0 {
+		return 0, fmt.Errorf("рост должен %.2f быть больше 0", height)
+	}
+	if duration <= 0 {
+		return 0, fmt.Errorf("длительность %v должна быть больше 0", duration)
+	}
+
+	countCaloriesRun := (weight * meanSpeed(steps, height, duration) * float64(duration.Minutes())) / minInH
+
+	return countCaloriesRun, nil
 }
 
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	// TODO: реализовать функцию
+	if steps <= 0 {
+		return 0, fmt.Errorf("количество шагов %d должно быть больше 0", steps)
+	}
+	if weight <= 0 {
+		return 0, fmt.Errorf("вес %.2f должен быть больше 0", weight)
+	}
+	if height <= 0 {
+		return 0, fmt.Errorf("рост %.2f должен быть больше 0", height)
+	}
+	if duration <= 0 {
+		return 0, fmt.Errorf("длительность %v должна быть больше 0", duration)
+	}
+
+	countCaloriesWalk := (weight * meanSpeed(steps, height, duration) * float64(duration.Minutes())) / minInH * walkingCaloriesCoefficient
+	return countCaloriesWalk, nil
 }
